795b31fd57934bc9a1064b7196d90653ef57bafd
汪浩浩 <whh881114@gmail.com>
2025-04-15 22:05:05 +0800

    修改版本号

--- alertmanager-qywx-bot/build.sh
+++ alertmanager-qywx-bot/build.sh
@@ -2,7 +2,7 @@
 
 registry_url="harbor.idc.roywong.work"
 registry_project="library"
-image="$registry_url"/"$registry_project"/alertmanager-qywx-bot:v1.01
+image="$registry_url"/"$registry_project"/alertmanager-qywx-bot:v1.02
 
 docker build -t $image .
 docker push $image
