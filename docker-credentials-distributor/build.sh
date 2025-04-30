5f59dac54c5d88d85ff73c53fb7ce8aaafd50672
汪浩浩 <whh881114@gmail.com>
2025-04-30 17:49:29 +0800

    修改镜像地址

--- docker-credentials-distributor/build.sh
+++ docker-credentials-distributor/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 registry_url="harbor.idc.roywong.work"
-registry_project="library"
+registry_project="public"
 image="$registry_url"/"$registry_project"/docker-credentials-distributor:latest
 
 docker build -t $image .
