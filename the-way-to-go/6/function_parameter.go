1f6bc1802f773ab422943f48c0d21d33cea81dbd
汪浩浩 <whh881114@gmail.com>
2025-06-09 15:08:19 +0800

    the-way-to-go: chapter 6

---
+++ the-way-to-go/6/function_parameter.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	callback(1, Add)
+
+}
+
+func Add(a, b int) {
+	fmt.Printf("%d + %d = %d\n", a, b, a+b)
+}
+
+func callback(y int, f func(int, int)) {
+	f(y, 2)
+}
