f0420d551271777b473827407ae362be32dde307
汪浩浩 <whh881114@gmail.com>
2025-06-10 09:58:38 +0800

    the-way-to-go: chapter 8

---
+++ the-way-to-go/8/maps_forrange2.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	items := make([]map[int]int, 5)
+	for i := range items {
+		items[i] = make(map[int]int, 1)
+		items[i][1] = 2
+	}
+	fmt.Printf("Version A: value of items: %v\n", items)
+
+	items2 := make([]map[int]int, 5)
+	for _, item := range items2 {
+		item = make(map[int]int, 1)
+		item[1] = 2
+	}
+	fmt.Printf("Version B: value of items: %v\n", items2)
+}
