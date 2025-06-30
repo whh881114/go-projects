afdc7eef6927bf3e18289082b0e40a4cce24b75b
汪浩浩 <whh881114@gmail.com>
2025-06-30 17:39:12 +0800

    the-way-to-go: 编程练习题

---
+++ the-way-to-go/city-temperature-average.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type CityTemperature struct {
+	City         any       `json:"city"`
+	Temperatures []float64 `json:"temperatures"`
+}
+
+func main() {
+	data, err := os.ReadFile("temperature.json")
+	if err != nil {
+		fmt.Println("读取文件失败：", err)
+		return
+	}
+
+	var cities []CityTemperature
+	if err := json.Unmarshal(data, &cities); err != nil {
+		fmt.Println("JSON文件解析失败：", err)
+		return
+	}
+
+	for _, city := range cities {
+		sum := 0.0
+		for _, temp := range city.Temperatures {
+			sum += temp
+		}
+		avg := sum / float64(len(cities))
+		fmt.Printf("%s: %.2f\n", city.City, avg)
+	}
+}
