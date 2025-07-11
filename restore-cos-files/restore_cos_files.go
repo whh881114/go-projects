76532851fa22c33d101c65ed7ba5843e7f857f27
汪浩浩 <whh881114@gmail.com>
2025-07-11 16:38:12 +0800

    restore_cos_files.sh脚本使用go重构

--- restore-cos-files/restore_cos_files.go
+++ restore-cos-files/restore_cos_files.go
@@ -88,6 +88,22 @@ func main() {
 	logrus.Info("所有归档文件处理完毕。")
 }
 
+func loadConfig(path string) (*Config, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	decoder := yaml.NewDecoder(file)
+	var cfg Config
+	if err := decoder.Decode(&cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
+
 func scanAndSendObjects(client *cos.Client, cfg *Config, prefix, date string, out chan<- string) {
 	opt := &cos.BucketGetOptions{
 		Prefix:  prefix,
@@ -123,9 +139,7 @@ func restoreObject(client *cos.Client, key string, days, workerID int, dryRun bo
 
 	opt := &cos.ObjectRestoreOptions{
 		Days: days,
-		CASJobParameters: &cos.CASJobParameters{
-			Tier: "Standard",
-		},
+		Tier: "Standard", // 选择恢复类型
 	}
 
 	_, err := client.Object.PostRestore(context.Background(), key, opt)
@@ -134,20 +148,4 @@ func restoreObject(client *cos.Client, key string, days, workerID int, dryRun bo
 	} else {
 		logrus.Infof("[Worker %d] 恢复成功: %s", workerID, key)
 	}
-}
-
-func loadConfig(path string) (*Config, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	decoder := yaml.NewDecoder(file)
-	var cfg Config
-	if err := decoder.Decode(&cfg); err != nil {
-		return nil, err
-	}
-
-	return &cfg, nil
 }
